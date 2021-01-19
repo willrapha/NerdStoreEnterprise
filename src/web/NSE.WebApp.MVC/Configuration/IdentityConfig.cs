be9d213efac4ab847b02426a32a4a610185a3c5d
Willian Raphael Faria Matos <willian.matos@ms.senac.br>
2021-01-19 14:06:04 -0400

    Web - Configurando a autenticação

---
+++ src/web/NSE.WebApp.MVC/Configuration/IdentityConfig.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NSE.WebApp.MVC.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static void AddIdentityConfiguration(this IServiceCollection services)
+        {
+            // Adicionado suporte a Cookie
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/login"; // Caminho para realizar login
+                    options.AccessDeniedPath = "/acesso-negado";
+                });
+        }
+
+        public static void UseIdentityConfiguration(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
