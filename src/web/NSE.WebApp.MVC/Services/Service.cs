6df1832fad9422eb4e521c7147ed2302b078d1ef
Willian Raphael Faria Matos <willian.matos@ms.senac.br>
2021-01-20 16:09:57 -0400

    Web - Boas práticas com HttpServices - pt I

--- src/web/NSE.WebApp.MVC/Services/Service.cs
+++ src/web/NSE.WebApp.MVC/Services/Service.cs
@@ -1,14 +1,32 @@
 ﻿using NSE.WebApp.MVC.Extensions;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace NSE.WebApp.MVC.Services
 {
     public abstract class Service
     {
+        protected StringContent ObterConteudo(object dado)
+        {
+            return new StringContent(
+                 JsonSerializer.Serialize(dado),
+                 Encoding.UTF8,
+                 "application/json");
+        }
+
+        protected async Task<T> DeserializarObjetoResponse<T>(HttpResponseMessage responseMessage)
+        {
+            // Remover Case Sensitive
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            return JsonSerializer.Deserialize<T>(await responseMessage.Content.ReadAsStringAsync(), options);
+        }
+
         protected bool TratarErrosResponse(HttpResponseMessage response)
         {
             switch (response.StatusCode.GetHashCode())
