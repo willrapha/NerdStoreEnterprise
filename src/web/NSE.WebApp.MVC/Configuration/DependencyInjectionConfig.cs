ef696747658cf14e6ce7cc8c9b69055678096ab8
Willian Raphael Faria Matos <willian.matos@ms.senac.br>
2021-01-28 13:18:10 -0400

    API - Retry Pattern com Polly

--- src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -4,6 +4,8 @@ using Microsoft.Extensions.DependencyInjection;
 using NSE.WebApp.MVC.Extensions;
 using NSE.WebApp.MVC.Services;
 using NSE.WebApp.MVC.Services.Handlers;
+using Polly;
+using Polly.Extensions.Http;
 using System;
 
 namespace NSE.WebApp.MVC.Configuration
@@ -19,17 +21,46 @@ namespace NSE.WebApp.MVC.Configuration
             // HttpClient atraves do meu HttpClientFactory
             services.AddHttpClient<IAutenticacaoService, AutenticacaoService>();
 
-            //services.AddHttpClient<ICatalogoService, CatalogoService>()                                                                                                       
+            //services.AddHttpClient<ICatalogoService, CatalogoService>()
             //    .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>(); // HttpClientAuthorizationDelegatinhHandler - Handler que criamos para manipular o request
 
+            #region Polly Especializada
+            //var retryWaitPolicy = HttpPolicyExtensions
+            //    .HandleTransientHttpError()
+            //    .WaitAndRetryAsync(new[]
+            //    {
+            //        TimeSpan.FromSeconds(1),
+            //        TimeSpan.FromSeconds(5),
+            //        TimeSpan.FromSeconds(10)
+            //    }, (outcome, timespan, retryCount, context) => // onRetry, a cada tentativa ele faz determinada ação no nosso caso exibe ao console
+            //    { 
+            //        Console.ForegroundColor = ConsoleColor.Blue;
+            //        Console.WriteLine($"Tentando pela {retryCount} vez!");
+            //        Console.ForegroundColor = ConsoleColor.White;
+            //    });
+
+            //services.AddHttpClient<ICatalogoService, CatalogoService>()
+            //    .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+            //    .AddPolicyHandler(retryWaitPolicy);
+            #endregion
+
+            #region Polly Simples
+            //services.AddHttpClient<ICatalogoService, CatalogoService>()
+            //    .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>() 
+            //    .AddTransientHttpErrorPolicy(   // Polly - Politica para erro HTTP
+            //        p => p.WaitAndRetryAsync(3, _ => TimeSpan.FromMilliseconds(600)));
+            #endregion
+
+            #region Refit
             // Registrando dependencia Refit
-            services.AddHttpClient("Refit", 
-            options => 
-            {
-                options.BaseAddress = new Uri(configuration.GetSection("CatalogoUrl").Value);
-            })
-            .AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
-            .AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>);
+            //services.AddHttpClient("Refit", 
+            //options => 
+            //{
+            //    options.BaseAddress = new Uri(configuration.GetSection("CatalogoUrl").Value);
+            //})
+            //.AddHttpMessageHandler<HttpClientAuthorizationDelegatingHandler>()
+            //.AddTypedClient(Refit.RestService.For<ICatalogoServiceRefit>);
+            #endregion
 
             // Singleton - porque utilizamos para a aplicação toda
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
