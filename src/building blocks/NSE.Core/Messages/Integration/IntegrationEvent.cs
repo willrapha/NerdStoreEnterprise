34f04656ac0caf3060f57edd2c1eb6df4c52b949
Willian Raphael Faria Matos <willian.matos@ms.senac.br>
2021-02-01 11:03:03 -0400

    RabbitMQ - Request Response Pattern

---
+++ src/building blocks/NSE.Core/Messages/Integration/IntegrationEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NSE.Core.Messages.Integration
+{
+    // Classe base para os eventos de integração
+    public abstract class IntegrationEvent : Event
+    {
+
+    }
+
+    // Estamos colocando essa classe no Core pq ela pode tbm ser utilizada pelo Cliente
+    public class UsuarioRegistradoIntegrationEvent : IntegrationEvent
+    {
+        public Guid Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Email { get; private set; }
+        public string Cpf { get; private set; }
+
+        // Esse usuario vai se tornar um cliente
+        public UsuarioRegistradoIntegrationEvent(Guid id, string nome, string email, string cpf)
+        {
+            Id = id;
+            Nome = nome;
+            Email = email;
+            Cpf = cpf;
+        }
+    }
+}
---
+++ src/building blocks/NSE.Core/Messages/Integration/ResponseMessage.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation.Results;
+
+namespace NSE.Core.Messages.Integration
+{
+    public class ResponseMessage : Message
+    {
+        // Utilizamos o proprio objeto do FluentValidation pois ja possui um metodo de validação e nossa lista de erros
+        public ValidationResult ValidationResult { get; set; }
+
+        public ResponseMessage(ValidationResult validationResult)
+        {
+            ValidationResult = validationResult;
+        }
+    }
+}
