7484d6a63f53b768476caf7943ffc3fcdf200509
Willian Raphael Faria Matos <willian.matos@ms.senac.br>
2021-04-12 20:49:10 -0400

    Pedido - Consumindo serviços pelo BFF - pt II

--- src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/NSE.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -86,7 +86,7 @@ namespace NSE.WebApp.MVC.Configuration
         }
     }
 
-    public class PollyExtensions
+    public static class PollyExtensions
     {
         public static AsyncRetryPolicy<HttpResponseMessage> EsperarTentar()
         {
