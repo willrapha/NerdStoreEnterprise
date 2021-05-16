f87cd46c26e510791b1c302b115d2945db82f7d3
Willian Raphael Faria Matos <willian.matos@ms.senac.br>
2021-05-16 13:40:39 -0400

    Pedido Camada de application de pedido - pt I

---
+++ src/services/NSE.Pedidos.API/Application/Commands/PedidoCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation.Results;
+using MediatR;
+using NSE.Core.Messages;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NSE.Pedidos.API.Application.Commands
+{
+    public class PedidoCommandHandler : CommandHandler, IRequestHandler<AdicionarPedidoCommand, ValidationResult>
+    {
+        public Task<ValidationResult> Handle(AdicionarPedidoCommand message, CancellationToken cancellationToken)
+        {
+            // Validação do comando
+
+            // Mapear Pedido
+
+            // Aplicar voucher se houver
+
+            // Validar pedido
+
+            // Processar pagamento
+
+            // Se pagamento tudo ok!
+
+            // Adicionar Evento
+
+            // Adicionar pedido Repositorio
+
+            // Persistir dados de pedido e voucher
+        }
+    }
+}
